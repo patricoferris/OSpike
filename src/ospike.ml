5141bea19e62d8912321b191711f113d54cb5093
patricoferris <patrickferris17@gmail.com>
2020-03-29 12:10:23 +0100

    Fix instruction counting, refactors stats module

--- src/ospike.ml
+++ src/ospike.ml
@@ -8,11 +8,13 @@ let command =
         and _output = flag "-o" (optional string)  ~doc:"filename where the results should be stored"
         and lower  = flag "-lower" (optional int) ~doc:"lower-bound on the address range to include in the stats"
         and upper  = flag "-upper" (optional int) ~doc:"upper-bound on the address range to include in the stats"
-        and group  = flag "-group" (optional int) ~doc:"size of the groups of adjacent instruction to look at (default 1)"
+        and group  = flag "-group" (optional_with_default 1 int) ~doc:"size of the groups of adjacent instruction to look at (default 1)"
       in
         fun () -> 
           match mode with 
-            | Some "stdin" -> Stats.print_sorted (Parser.from_stdin {lower; upper; group})
+            | Some "stdin" -> 
+              let options : Parser.t = {lower; upper; group} in 
+                Parser.(print_sorted (from_stdin options) options)
             | Some "file"  -> print_endline "File"
             | None | Some _ -> print_endline "Please provide either build, spike-build, run, spike, log or clean as mode"
     )
--- src/parser.mli
+++ src/parser.mli
@@ -1,7 +1,7 @@
 type t = {
   lower: int option;
   upper: int option;
-  group: int option; 
+  group: int; 
 }
 
 val parse_line : string -> Riscv.t 
@@ -9,3 +9,9 @@ val parse_line : string -> Riscv.t
 
 val from_stdin : t -> (Riscv.t Buffer.t, int) Hashtbl.t
 (** Parses a log file from stdin - should be used by piping "spike -l $pk executable.out" to it *)
+
+val add_to_table : ('a, int) Hashtbl.t -> 'a -> unit 
+(** Adds an instruction to the hashtable incrementing (or initialising) the frequency *)
+
+val print_sorted : (Riscv.t Buffer.t, int) Hashtbl.t -> t -> unit 
+(** Prints the contents of the hashtable in sorted order *)
